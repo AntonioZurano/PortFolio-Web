8b9a13c4f192185b605391bfbeb6a013a738dc31
azuranob <azurano@outlook.es>
2023-11-01 20:35:50 +0100

    resize en js para el menu responsive

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -20,4 +20,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     });
 
+    window.addEventListener("resize", (event) => {
+        let size = parseInt(document.body.clientWidth);
+
+        if(size <= 1060) {
+            aside.classList.remove('layout__aside--visible');
+            icon_bars.style.opacity = 1;
+            icon_xmark.style.opacity = 0;
+        }
+    });
+
 });
