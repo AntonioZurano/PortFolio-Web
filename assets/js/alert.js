1071361c62de78375175889b6debee5efd6aabdb
azuranob <azurano@outlook.es>
2023-11-26 20:40:52 +0100

    add js for controller submit form

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -6,3 +6,31 @@ function mostrarAlerta() {
     console.log("Despues de la alerta")
 }
 
+document.querySelector('.contact__form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de la manera predeterminada
+
+    // Obtén los valores de los campos del formulario
+    var name = document.querySelector('input[name="name"]').value;
+    var email = document.querySelector('input[name="email"]').value;
+    var subject = document.querySelector('input[name="subject"]').value;
+    var message = document.querySelector('textarea[name="message"]').value;
+
+    // Valida los campos
+    if (name === '' || email === '' || subject === '' || message === '') {
+        alert('Todos los campos son obligatorios');
+        return;
+    }
+
+    // Valida el formato del email
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailRegex.test(email)) {
+        alert('El email no es válido');
+        return;
+    }
+
+    // Si todo está bien, puedes enviar el formulario manualmente
+    event.target.submit();
+
+    // Cambia la URL del navegador a la nueva página web
+    window.location.href = 'gracias.html';
+});
